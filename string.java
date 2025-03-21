096d93ddc8d978fda9a9e5d9bd3ffa7425c65846
Jerintom-10 <jerintom468@gmail.com>
2025-03-21 09:56:50 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class string
+{
+public static void main(String args[])
+{
+int n,i,j;
+String temp;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter no.of strings");
+n=sc.nextInt();
+String[] str=new String[n];
+System.out.println("enter the strings:");
+for(i=0;i<n;i++)
+{
+str[i]=sc.nextLine();
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(str[i].compareTo(str[j])>0)
+{
+temp=str[i];
+str[i]=str[j];
+str[j]=temp;
+}
+}
+}
+System.out.println("sorted strings:");
+for(i=0;i<n;i++)
+{
+System.out.println(str[i]);
+}
+}
+}
+
